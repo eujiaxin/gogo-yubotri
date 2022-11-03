5b91f722d2ee9e073cd67aed92d8d845548d24fa
heheheejin <e_jaxx@hotmail.com>
2022-11-03 08:01:05 +0800

    Update row with date and select data

--- notion/notion.js
+++ notion/notion.js
@@ -4,19 +4,57 @@ require("dotenv").config();
 const notion = new Client({ auth: process.env.NOTION_TOKEN });
 const databaseId = process.env.NOTION_DATABASE_ID;
 
-async function addItem(text) {
+async function addItem(text, isSelf = false) {
     try {
+        const today = new Date();
+        const rev1Date = new Date(today.getTime());
+        rev1Date.setDate(rev1Date.getDate() + 7);
+        const rev2Date = new Date(rev1Date.getTime());
+        rev2Date.setDate(rev2Date.getDate() + 7);
+
         const response = await notion.pages.create({
             parent: { database_id: databaseId },
             properties: {
-                title: {
+                Question: {
+                    type: "title",
                     title: [
                         {
+                            type: "text",
                             text: {
                                 content: text,
                             },
                         },
                     ],
+                },
+                Difficulty: {
+                    type: "select",
+                    select: {
+                        name: "Hard",
+                    },
+                },
+                isSelf: {
+                    type: "select",
+                    select: {
+                        name: "N",
+                    },
+                },
+                Date: {
+                    type: "date",
+                    date: {
+                        start: today.toISOString(),
+                    },
+                },
+                Revision1: {
+                    type: "date",
+                    date: {
+                        start: rev1Date.toISOString(),
+                    },
+                },
+                Revision2: {
+                    type: "date",
+                    date: {
+                        start: rev2Date.toISOString(),
+                    },
                 },
             },
         });
@@ -27,4 +65,4 @@ async function addItem(text) {
     }
 }
 
-addItem("LEETCODE 1");
+addItem("LEETCODE X");
